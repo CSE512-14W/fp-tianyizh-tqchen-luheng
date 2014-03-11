437b9fa36d3897513bfac148203868ad9920f778
tianyizhou <tianyi.david.zhou@gmail.com>
2014-03-11 16:09:07 -0700

    add feature

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ var enable_toggle = false;
 var btrees = new boosting_tree( { top:30, right:50, bottom:10, left:50}, 
         						900, 1000, "#modeltreegraph" , enable_toggle);
 
+//feature.push(["aaa", 1]);
+d3.json( "data/feature.json",
+         function( error, data){
+             var nodes = data.nodes;
+             var feature = [];
+             for (var i = 0; i < nodes.length; i++) {
+                 feature.push([nodes[i].feature, nodes[i].types, nodes[i].explanation]);
+                 console.log(nodes[i]);
+                 //feature.push(["dasd", 2]);
+             };
+             console.log(feature);
+             var featuretable = new TableSort(
+                "#featuretable",
+                [
+                    { text: 'Feature', sort: TableSort.alphabetic},
+                    { text: 'Type', sort: TableSort.numeric, sort_column: true },
+                    { text: 'Details', sort: TableSort.alphabetic}
+
+                ],
+                feature,
+                { width: '400px', height: '500px' }
+                );
+         }
+       );
 
 d3.json( "data/mushroom.json",
          function( error, data ){
