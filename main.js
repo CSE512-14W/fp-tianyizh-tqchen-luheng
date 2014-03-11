ddd533a7a7cd959ebb8b8b87415ba6168f8d8ac7
tianyizhou <tianyi.david.zhou@gmail.com>
2014-03-11 16:04:58 -0700

    feature table

--- main.js
+++ main.js
@@ -10,6 +10,29 @@ var btrees = new boosting_tree( { top:30, right:150, bottom:10, left:50},
 
 var init_request = {
 	op_type : "init", op_iter : 0, num_trees : 0 };
+	
+d3.json( "data/feature.json",
+         function( error, data){
+             var nodes = data.nodes;
+             var feature = [];
+             for (var i = 0; i < nodes.length; i++) {
+                 feature.push([nodes[i].feature, nodes[i].types, nodes[i].explanation]);
+                 console.log(nodes[i]);
+             };
+             console.log(feature);
+             var featuretable = new TableSort(
+                "#featuretable",
+                [
+                    { text: 'Feature', sort: TableSort.alphabetic},
+                    { text: 'Type', sort: TableSort.numeric, sort_column: true },
+                    { text: 'Details', sort: TableSort.alphabetic}
+
+                ],
+                feature,
+                { width: '400px', height: '500px' }
+                );
+         }
+       );
 
 $.get("cgi-bin/tree_manipulation.py", 
 		init_request,
