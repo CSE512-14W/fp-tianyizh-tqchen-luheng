8a2fa9c1060b55197459b6fa539857f32d0dfe4c
tqchen <tianqi.tchen@gmail.com>
2014-03-20 14:46:55 -0700

    new timl server

--- public/cgi-bin/request_handler.py
+++ public/cgi-bin/request_handler.py
@@ -6,76 +6,24 @@ import cgi
 import cgitb
 import imp
 import json
-import os
+import socket
 import sys
-import uuid
 
-#sys.path.append(os.path.dirname(__file__) + '../../bin/xgboost_utils.py')
-#import xgboost_utils
-
-xgboost_utils = imp.load_source('xgboost_utils',
-                                os.path.join(os.path.dirname(__file__),
-                                             '../../bin/xgboost_utils.py'))
-
-feature_utils = imp.load_source('feature_utils',
-                                os.path.join(os.path.dirname(__file__),
-                                             '../../bin/feature_utils.py'))
-
-def makeInt(obj):
-    if isinstance(obj, list):
-        return [int(o) for o in obj]
-    return int(obj)
-
+HOST, PORT = "localhost", 9911
 
 cgitb.enable()
-request = json.loads(cgi.FieldStorage()["request"].value)
-sys.stderr.write(str(request) + "\n")
-op_type = request["op_type"]
-op_iter = int(request["op_iter"])
-num_trees = int(request["num_trees"])
-dataset = request["dataset"]
+rawreq = cgi.FieldStorage()["request"].value;
+print rawreq
 
-new_config = []
-new_forest = None
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    # Connect to server and send data
+    sock.connect( (HOST, PORT) )
+    sock.sendall( rawreq+'\n' )
+    # Receive data from the server and shut down
+    received = sock.makefile().readline()
+    print 'Content-type: application/json\n\n' 
+    print received
+finally:
+    sock.close()
 
-xgboost_utils.setDataset(dataset)
-
-if op_type == "restore_op": 
-    user_id = request["user_id"]
-    new_forest = xgboost_utils.loadModel(user_id, op_iter)
-elif op_iter == 0:
-      # assign new user
-    user_id = uuid.uuid1()
-    new_config = [("num_round", num_trees),
-                  ("bst:max_depth", request["max_depth"])]
-    new_forest = xgboost_utils.trainNewModel(user_id, 0, new_config)
-else:
-    booster_id = makeInt(request["tree_id"])
-    node_id = makeInt(request["node_id"])
-    user_id = request["user_id"]
-    
-    if op_type == "node_expand":
-        new_config = [("interact:booster_index", booster_id),\
-                      ("bst:interact:expand", node_id)]
-    elif op_type == "node_remove":
-        new_config = [("interact:booster_index", booster_id),
-                      ("bst:interact:remove", node_id)]
-    elif op_type == "tree_expand":
-        new_config = [("num_round", num_trees), ]
-    elif op_type == "tree_remove":
-        new_config = [("interact:booster_index", booster_id),
-                      ("interact:action", "remove")]
-    elif op_type == "node_expand_all":
-        new_config = [("interact:booster_index", booster_id),\
-                      ("bst:interact:expand", node_id)]
-    elif op_type == "node_remove_all":
-        new_config = [("interact:booster_index", booster_id),\
-                      ("bst:interact:expand", node_id)]
-    new_forest = xgboost_utils.trainNewModel(user_id, op_iter, new_config)
-
-
-new_forest["op_iter"] = op_iter + 1
-new_forest["user_id"] = str(user_id)
-
-print 'Content-type: application/json\n\n' 
-print json.dumps(new_forest)
